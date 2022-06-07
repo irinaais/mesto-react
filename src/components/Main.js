5f17b7de8ed775d36a0c1da3280c471bc7f44981
Iris <irinaais90@yandex.ru>
2022-06-07 13:26:13 +0300

    feat: in Main.js add functions: handleEditAvatarClick, handleEditProfileClick, handleAddPlaceClick.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,18 @@
 import avatar from '../images/jack_iv_custo.jpg';
 
 function Main() {
+  function handleEditAvatarClick() {
+    document.querySelector('.popup_edit-avatar').classList.add('popup_opened');
+  }
+
+  function handleEditProfileClick() {
+    document.querySelector('.popup_edit').classList.add('popup_opened');
+  }
+
+  function handleAddPlaceClick() {
+    document.querySelector('.popup_add').classList.add('popup_opened');
+  }
+
   return(
     <>
       <main>
@@ -11,17 +23,17 @@ function Main() {
               src={avatar}
               alt="Аватарка"
             />
-            <button className="button button_variant_avatar" type="button" aria-label="Загрузить аватар"></button>
+            <button className="button button_variant_avatar" onClick={handleEditAvatarClick} type="button" aria-label="Загрузить аватар"></button>
             <div className="profile__info">
               <div className="profile__box">
                 <h1 className="profile__title">Жак-Ив Кусто</h1>
-                <button className="button button_variant_edit" type="button"
+                <button className="button button_variant_edit" onClick={handleEditProfileClick} type="button"
                         aria-label="Редактировать профиль"></button>
               </div>
               <p className="profile__subtitle">Исследователь океана</p>
             </div>
           </div>
-          <button className="button button_variant_add" type="button" aria-label="Добавить фотографии"></button>
+          <button className="button button_variant_add" onClick={handleAddPlaceClick} type="button" aria-label="Добавить фотографии"></button>
         </section>
 
         <section className="cards page__section">
